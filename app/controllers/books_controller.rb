8aab904e0fd8774dae0010c0b41f5589b409c79b
EC2 Default User <ec2-user@ip-172-31-19-167.ec2.internal>
2022-04-10 13:37:02 +0000

    [Finish]9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_comment = BookComment.new
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
   end
 
   def index
     @book = Book.new
-    to  = Time.current.at_end_of_day
-    from  = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort {|a,b|
-        b.favorited_users.includes(:favorites).where(created_at: from...to).size <=>
-        a.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }
+    @books = Book.all
   end
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :rooms, through: :user_rooms
+  has_many :view_counts, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
